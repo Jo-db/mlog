b0fb34d5e354d0e9e1242ded7c5be00aeef2739a
조다빈 <db01050@naver.com>
2026-05-08 22:01:16 +0900

    상세 페이지 연결

--- app.py
+++ app.py
@@ -4,15 +4,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', posts=posts)
 
 @app.route('/write')
 def write():
     return render_template('write.html')
 
-@app.route('/post')
-def post():
-    return render_template('post.html')
+@app.route('/post/<int:index>')
+def post(index):
+    post = posts[index]
+    return render_template('post.html', post=post)
+
+posts = [
+    {'title': '첫 번째 글','content': '이것은 첫 번째 글의 내용입니다.'},
+    {'title': '두 번째 글','content': '이것은 두 번째 글의 내용입니다.'},
+]
 
 
 if __name__ == '__main__':
